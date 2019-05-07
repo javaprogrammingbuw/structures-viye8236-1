f5b51723c0bb23108baa58085e74b335a3537130
unknown <justinjames005@gmail.com>
2019-05-07 17:47:48 +0200

    Even and Odd

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
 
-//loops
+
 
 public class EvenOdd {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+		int n = scan.nextInt();
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
+		System.out.println("Enter the values");
+		int a = scan.nextInt();
+		//loops
+		for (int i=0; i<a; i++) {
+			if (i%2 != 0) {
+				System.out.println(i+" The odd numbers are");
+			}
+		}
 		
 	}
 	
 }
+
